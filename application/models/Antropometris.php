039c98c0970f4b33b92a79904af37478222d5f0b
Henri Susanto <henry.dinus@gmail.com>
2020-06-29 04:44:45 +0700

    kalkulator gizi

--- application/models/Antropometris.php
+++ application/models/Antropometris.php
@@ -50,4 +50,21 @@ class Antropometris extends MY_Model
       ->select('antropometri.item');
     return parent::dt();
   }
+
+  function gizi($jenis_kelamin, $tgl_lahir, $bb, $tb)
+  {
+    $found = $this->db->query("
+        SELECT hasil
+        FROM antropometri
+        WHERE nama = 'Gizi {$jenis_kelamin}'
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 >= usia_min
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 <= usia_max
+        AND FLOOR(({$tb} / 0.5) * 0.5) >= tb_min
+        AND FLOOR(({$tb} / 0.5) * 0.5) <= tb_max
+        AND {$bb} >= bb_min
+        AND {$bb} <= bb_max
+      ")
+      ->row_array();
+    return $found ? $found['hasil'] : 'Formula tidak ditemukan';
+  }
 }
