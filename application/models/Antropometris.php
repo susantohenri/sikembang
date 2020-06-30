6cebdb9f0fa91674b068915e61f8cde1bb75508d
Henri Susanto <henry.dinus@gmail.com>
2020-07-01 06:35:15 +0700

    kalkulator done

--- application/models/Antropometris.php
+++ application/models/Antropometris.php
@@ -51,6 +51,36 @@ class Antropometris extends MY_Model
     return parent::dt();
   }
 
+  function bb($jenis_kelamin, $tgl_lahir, $bb, $tb)
+  {
+    $found = $this->db->query("
+        SELECT hasil
+        FROM antropometri
+        WHERE nama = 'Berat Badan {$jenis_kelamin}'
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 >= usia_min
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 <= usia_max
+        AND {$bb} >= bb_min
+        AND {$bb} <= bb_max
+      ")
+      ->row_array();
+    return $found ? $found['hasil'] : 'Formula tidak ditemukan';
+  }
+
+  function tb($jenis_kelamin, $tgl_lahir, $bb, $tb)
+  {
+    $found = $this->db->query("
+        SELECT hasil
+        FROM antropometri
+        WHERE nama = 'Tinggi Badan {$jenis_kelamin}'
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 >= usia_min
+        AND DATEDIFF(CURRENT_DATE, '{$tgl_lahir}') / 30 <= usia_max
+        AND FLOOR(({$tb} / 0.5) * 0.5) >= tb_min
+        AND FLOOR(({$tb} / 0.5) * 0.5) <= tb_max
+      ")
+      ->row_array();
+    return $found ? $found['hasil'] : 'Formula tidak ditemukan';
+  }
+
   function gizi($jenis_kelamin, $tgl_lahir, $bb, $tb)
   {
     $found = $this->db->query("
--- application/views/frontpage/kalkulator.php
+++ application/views/frontpage/kalkulator.php
@@ -1,35 +1,30 @@
-<?php if ('form' === $view_switch) : ?>
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/select2.min.css') ?>">
     <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap-datepicker.css') ?>">
-    <form action="" method="POST" class="main-form col-sm-12">
+    <form action="" method="POST" class="main-form col-sm-12" id="form_kalkulator">
         <div class="card card-warning card-outline">
             <div class="card-body">
 
-                <div class="row">
-                    <div class="col-sm-12 text-right">
-                        <button class="btn btn-info btn-save"><i class="fa fa-calculator"></i> &nbsp; Hitung</button>
-                        <a href="<?= site_url('Frontpage') ?>" class="btn btn-warning"><i class="fa fa-arrow-left"></i> &nbsp; Batal</a>
-                    </div>
-                </div>
-                <br>
-
                 <div class="" data-controller="<?= $current['controller'] ?>">
                     <div class="form-horizontal form-groups">
-                        <input type="hidden" name="last_submit" value="<?= time() ?>">
 
                         <div class="form-group row">
-                            <label class="col-sm-3 control-label">Nama</label>
+                            <label class="col-sm-3 control-label">Jenis Perhitungan</label>
                             <div class="col-sm-9">
-                                <input class="form-control" type="text" value="" name="nama">
+                                <select name="jenis_kalkulator" class="form-control">
+                                    <option value="bb">Berat Badan / Usia</option>
+                                    <option value="tb">Tinggi Badan / Usia</option>
+                                    <option value="bbtb">Tinggi Badan / Tinggi Badan</option>
+                                </select>
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label class="col-sm-3 control-label">Jenis Kelamin</label>
                             <div class="col-sm-9">
-                                <input type="radio" name="jenis_kelamin" checked="checked"> Lelaki
-                                &nbsp;&nbsp;&nbsp;
-                                <input type="radio" name="jenis_kelamin"> Perempuan
+                                <select name="jenis_kelamin" class="form-control">
+                                    <option value="Lelaki">Lelaki</option>
+                                    <option value="Perempuan">Perempuan</option>
+                                </select>
                             </div>
                         </div>
 
@@ -57,29 +52,60 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <div class="col-sm-12 text-right">
+                        <a id="submit" class="btn btn-info btn-save"><i class="fa fa-calculator"></i> &nbsp; Hitung</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </form>
-<?php else : ?>
     <div class="col-sm-12">
         <div class="card card-warning card-outline">
             <div class="card-body">
 
                 <div class="row">
                     <div class="col-sm-12 text-center">
-                        <h1><b>Nama Anak,</b><i style="color: #ffab00"> Gizi Baik</i></h1>
-                    </div>
-                </div>
-
-                <hr>
-                <div class="row">
-                    <div class="col-sm-12 text-right">
-                        <a href="" class="btn btn-info"><i class="fa fa-calculator"></i> &nbsp; Hitung Lagi</a>
-                        <a href="<?= site_url('Frontpage') ?>" class="btn btn-warning"><i class="fa fa-arrow-left"></i> &nbsp; Kembali</a>
+                        <h1><b>Hasil Perhitungan, </b><i style="color: #ffab00" id="result"></i></h1>
                     </div>
                 </div>
 
             </div>
         </div>
     </div>
-<?php endif ?>
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function() {
+            let bb = $('[name="bb"]').parent().parent()
+            let tb = $('[name="tb"]').parent().parent()
+            $('[name="jenis_kalkulator"]').change(function() {
+                bb.hide().find('input').val('')
+                tb.hide().find('input').val('')
+                $('#result').html('belum tersedia')
+                switch ($(this).val()) {
+                    case 'bb':
+                        bb.show();
+                        break
+                    case 'tb':
+                        tb.show();
+                        break
+                    case 'bbtb':
+                        bb.show()
+                        tb.show();
+                        break
+                }
+            }).val('bb').trigger ('change')
+
+            $('#submit').click(function() {
+                var data = {}
+                $('#form_kalkulator')
+                    .find('input, select')
+                    .each(function() {
+                        data[$(this).attr('name')] = $(this).val()
+                    })
+                $.post('', data, function(result) {
+                    $('#result').html(result)
+                })
+            })
+        });
+    </script>
